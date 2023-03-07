abb6eb11ae3924c802c5525adb5529359f2f2843
Derek Caldwell <djohncaldwell@gmail.com>
2023-03-07 14:38:50 -0800

    matching dev file structures

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import FindGame from "./components/FindGame";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from "./components/pages/Login"
+import FindGame from "./components/pages/Find-Game";
+import "./components/Find-Game/style.css"
 
 function App() {
-  return <FindGame />;
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <h1>Navbar</h1>
+      <br/>
+      <Routes>
+        <Route path="/" element={<h1>Homepage</h1>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<h1>Signup</h1>}/>
+        <Route path="/findfriend" element={<h1>Find Friend</h1>}/>
+        <Route path="/findgames" element={<FindGame/>}/>
+        <Route path="/mygroup" element={<h1>Group Page</h1>}/>
+        <Route path="/mylist" element={<h1>My List</h1>}/>
+        <Route path="*" element={<h1>404 page not found</h1>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/pages/Find-Game/index.js
+++ src/components/pages/Find-Game/index.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import GenerateCard from './GenerateCard'
-import "../styling/FindGame.css"
 
 function FindGame() {
     const [cards, setCards] = useState([]);
