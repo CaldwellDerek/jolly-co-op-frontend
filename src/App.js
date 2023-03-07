354d70cff3780013361b79356a0c60c7fbaeb57b
Claire <claire.3.eberle@gmail.com>
2023-03-07 11:39:05 -0800

    removed react  default files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Homepage</h1>}/>
         <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/signup" element={<h1>Signup</h1>}/>
+        <Route path="/findfriend" element={<h1>Find Friend</h1>}/>
         <Route path="/mygroup" element={<h1>Group Page</h1>}/>
+        <Route path="/mylist" element={<h1>My List</h1>}/>
         <Route path="*" element={<h1>404 page not found</h1>}/>
       </Routes>
       </BrowserRouter>
