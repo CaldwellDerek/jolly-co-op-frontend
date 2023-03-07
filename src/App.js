823e44af033fe8e0b84eccc520f558b353808401
Claire <claire.3.eberle@gmail.com>
2023-03-07 11:01:10 -0800

    added app routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <h1>Navbar</h1>
+      <br/>
+      <Routes>
+        <Route path="/" element={<h1>Homepage</h1>}/>
+        <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/mygroup" element={<h1>Group Page</h1>}/>
+        <Route path="*" element={<h1>404 page not found</h1>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
