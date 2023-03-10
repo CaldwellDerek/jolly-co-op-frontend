71d8dbccdc334882e3099d475169ced32758998d
Connor Mclaughlin <connormclaughlin@Connors-MacBook-Pro.local>
2023-03-10 09:31:23 -0800

    Start of fetch post group & css"

--- src/components/pages/Find-Friend/index.js
+++ src/components/pages/Find-Friend/index.js
@@ -4,9 +4,37 @@ import './style.css'
 import API from "../../../utils/API"
 const members = [];
 
+const showMembers = [];
+// create function for group name
+// fetch post to Groups to create group id and include group owners token
+// hide the enter Group name, show search users.
+
+
+
+
 const FindFriend = () => {
     const [data, setData] = useState([]);
     const [query, setQuery] = useState("");
+
+    const addMember = (e) => {
+        e.preventDefault();
+        // console.log(e.target.getAttribute('data-user'))
+        // console.log(e.target.getAttribute('data-username'))
+        members.push(e.target.getAttribute('data-user'));
+        showMembers.push(e.target.getAttribute('data-username'))
+        console.log(members)
+        console.log(showMembers)
+
+
+
+
+        return
+        // pull the users info from on click button
+        // push the group id & group token to the user
+        // fetch request group members in members area
+
+    }
+
 
     const fetchFriends = (e) => {
         // setQuery(e.target.value)
@@ -20,58 +48,58 @@ const FindFriend = () => {
             })
             console.log(newUsers)
             setData(newUsers);
-            document.querySelector('#add-member').addEventListener('click', e => {
-                members.push(data);
-            })
         }
         fetchUsers()
+        addMember()
         return
+
+
     }
+    return (
 
-
-return (
-
-    <div>
-        <div className="Search">
-            <h1 className="Search-Title">Search for friends</h1>
-            <div className="SearchBar">
-                <h3>Enter username to add friends to group</h3>
-
-                <input className="search" onChange={fetchFriends} placeholder="Search by name" />
-                {/* {<UserTable data={[data]}/>} */}
-                <table>
-                    <tbody>
-                        <tr>
-                            <th> Username:</th>
-                        </tr>
-                        {data.map((users) => (
-                            <tr key={users.id}>
-                                <th>{users.username}<button id="add-member">+</button>
-                                </th>
-                            </tr>
-                        )
-                        )}
-                    </tbody>
-                </table>
-
-            </div>
-        </div>
-
-        <div className="Group-Card">
-            <div>
+        <div className="search-page">
+            <div className="group-title">
                 <h3>group name:<input className="GroupName" id="group-name" placeholder="enter name of group"></input> </h3>
+                <button className="start-group">Start Group</button>
             </div>
 
-            <div>
-                {/* <h3>added members</h3>
-                    <tr key={members.id}>
-                        <th>{members.username}<button className="delete-member">+</button></th>
-                    </tr>  */}
+            <div className="Search">
+                <h1 className="Search-Title">Search for friends</h1>  
+                    <br />
+                    <input className="search" onChange={fetchFriends} placeholder="Search by name" />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th> Results</th>
+                            </tr>
+                            {data.map((users) => (
+                                <tr>
+                                    <th >{users.username}<button data-user={[users.id]} data-username={[users.username]} className='userInfo' id="add-member" onClick={addMember} >+</button>
+                                    </th>
+                                </tr>
+                            )
+                            )}
+                        </tbody>
+                    </table>
             </div>
-            <button>submit</button>
+
+            <div className="Group-Card">
+                <div>
+                    <h3>added members</h3>
+                    <table>
+                        <tbody>
+                            {showMembers.map((value) => {
+                                <tr>
+                                    <th><button className="delete-member">-</button></th>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <button>add games</button>
+            </div>
         </div>
 
-    </div>
-)
+    )
 }
 export default FindFriend
