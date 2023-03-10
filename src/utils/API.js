222af80bc05658ea2ed095e612c99e3f5f4b0a0f
Derek Caldwell <djohncaldwell@gmail.com>
2023-03-10 09:18:21 -0800

    display game to page, delete functionality

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,21 +2,22 @@ const URL_PREFIX="http://localhost:3001"
 
 const API = {
 
-getUserData:(id, token)=>{
-    return fetch(`${URL_PREFIX}/api/users/${id}`, {
-        headers:{
-            "authorization":`Bearer ${token}`
-        }
-    }).then(res=>res.json())
+    getUserData:(id, token)=>{
+        return fetch(`${URL_PREFIX}/api/users/${id}`, {
+            headers:{
+                "authorization":`Bearer ${token}`
+            }
+        }).then(res=>res.json())
 
-},
-isValidToken:token=>{
-    return fetch(`${URL_PREFIX}/api/users/isValidToken`, {
-        headers:{
-            "authorization":`Bearer ${token}`
-        }
-    }).then(res=>res.json())
-},
+    },
+
+    isValidToken:token=>{
+        return fetch(`${URL_PREFIX}/api/users/isValidToken`, {
+            headers:{
+                "authorization":`Bearer ${token}`
+            }
+        }).then(res=>res.json())
+    },
 
     login:userObj=>{
         return fetch(`${URL_PREFIX}/api/users/login`,{
@@ -27,6 +28,7 @@ isValidToken:token=>{
         }        
     }).then(res=>res.json())
     },
+
     signup:userObj=>{
         return fetch(`${URL_PREFIX}/api/users/signup`, {
             method:"POST",
@@ -36,9 +38,11 @@ isValidToken:token=>{
         }        
         }).then(res=>res.json())
     },
+
     getAllGroups: () =>{
         return fetch(`${URL_PREFIX}/api/groups`).then(res=>res.json())
     },
+
     saveGame: (gameObj, token)=> {
         return fetch(`${URL_PREFIX}/api/games`, {
             method:"PUT",
@@ -48,6 +52,15 @@ isValidToken:token=>{
                 "authorization": `Bearer ${token}`
             }
         }).then(res => res.json())
+    },
+
+    deleteGame: (id, token) => {
+        return fetch(`${URL_PREFIX}/api/games/${id}`, {
+            method: "DELETE",
+            headers: {
+                "authorization": `Bearer ${token}`
+            }
+        }).then(res => res.json());
     } 
 }
 
